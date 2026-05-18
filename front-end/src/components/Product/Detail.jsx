49dd2bb6c20a623546b1a111e72b86937b14e0ee
StreetFly <ryannitroo@gmail.com>
2026-05-17 22:19:44 -0700

    updated the category and subcategory models, controllers, and added the necessary endpoints for managing categories and subcategories. Also updated the product detail and list components to display category information.

--- front-end/src/components/Product/Detail.jsx
+++ front-end/src/components/Product/Detail.jsx
@@ -1,7 +1,14 @@
+import { useState, useEffect } from "react";
+import { useParams} from "react-router";
+
 export default function ProductDetail() {
+    const { id } = useParams();
+    console.log("Product ID:", id);
+
+
     return (
         <div>
-            <h2>Product Detail</h2>
+            <h2>Detail Product</h2>
             <p>Product details will be displayed here.</p>
         </div>
     );
